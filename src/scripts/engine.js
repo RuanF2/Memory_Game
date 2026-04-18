6939454c5ae7cbf102d4678c5544c12abb434338
Ruan <ruanfrancelino459@gmail.com>
2026-04-18 15:42:20 -0300

    Memory_Game

---
+++ src/scripts/engine.js
@@ -0,0 +1,65 @@
+ const emojis = [
+    "👻",
+    "👻",
+    "🎃",
+    "🎃",
+    "😎",
+    "😎",
+    "💵",
+    "💵",
+    "👀",
+    "👀",
+    "🙄",
+    "🙄",
+    "😝",
+    "😝",
+    "💗",
+    "💗",
+ ];
+ let openCards = [];
+ let contadorTentativas = 0;
+
+
+let shuffleEmojis = emojis.sort(() =>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement
+    ("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length ==2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList.remove("boxOpen")
+        openCards[1].classList.remove("boxOpen")
+
+        contadorTentativas++;
+        document.getElementById("tentativas").innerHTML = contadorTentativas;
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        const titulo = document.getElementById("titulo");
+        titulo.innerHTML = `VOCÊ VENCEU COM ${contadorTentativas} TENTATIVAS!`;
+        titulo.classList.add("vitoria-animada");
+    }
+}
+
