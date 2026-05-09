8aff7f3cac0ff2995330688dbcdc2b7ec21e2cc9
Ruan <ruanfrancelino459@gmail.com>
2026-05-09 16:03:53 -0300

    Aumento de tempo antes das cartas virarem

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -35,7 +35,7 @@ setTimeout(() => {
    document.querySelectorAll(".item").forEach(card => {
     card.classList.remove("boxOpen")
    })
-}, 700)
+}, 1000)
 
 function handleClick(){
     if(openCards.length < 2 && !this.classList.contains("boxOpen") && !this.classList.contains("boxMatch")){
